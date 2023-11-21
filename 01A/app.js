6ad743ee7894bd2fddbe9340915b28d2deb727a6
Elliot Enahoro <37766@ma-web.nl>
2023-11-21 12:48:06 +0100

    update2

--- 01A/app.js
+++ 01A/app.js
@@ -3,7 +3,20 @@ class App
     runnApplication()
     {
         console.log("hello world!")
-        //code gaat hier!
+        let appNaam = "appName"
+        let versienummer = 0.1
+        let versiedatum = "21-11-23"
+        let autheur = "Elliot"
+        let copyright = "MediaCollege"
+        let distributeur = "visualStudioCode"
+        let darkmode = false;
+    
+console.log (appNaam)
+console.log (versienummer)
+console.log(autheur)
+console.log(copyright)
+console.log(distributeur)
+console.log(darkmode)
     }
 }
 
