69d9aee33950c973411a3c64a66c0ab69cb4012d
Elliot075 <144677315+Elliot075@users.noreply.github.com>
2023-12-05 12:48:54 +0100

    Add files via upload

---
+++ 03start/app.js
@@ -0,0 +1,13 @@
+class App
+{
+    runnApplication()
+    {
+        document.getElementById("newstitle")
+        document.getElementsByClassName("gamenews headline")
+        document.getElementsByClassName("gamenews")
+    
+    }
+}
+
+let app = new App();
+app.runnApplication();
