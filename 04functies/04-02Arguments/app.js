f923c04f31cb1f561b82888ec98779ce8c3b2e47
Elliot Enahoro <37766@ma-web.nl>
2024-01-16 13:08:49 +0100

    change

---
+++ 04functies/04-02Arguments/app.js
@@ -0,0 +1,17 @@
+
+function argumentsAreHandy(shoutout)
+{
+    console.log("do you want to give a shoutout?");
+    console.log(shoutout);
+
+}
+
+argumentsAreHandy("Shout out to my friends");
+argumentsAreHandy("Shout out to my friends");
+argumentsAreHandy("Shout out to my friends");
+
+
+function superMooiGlobalFunction()
+{
+
+}
