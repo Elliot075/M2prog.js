8efb3eea39814c2443f64b8fbaee80977df9af3c
Elliot Enahoro <37766@ma-web.nl>
2024-01-25 12:18:29 +0100

    finish

---
+++ 04functies/04-04class/app.js
@@ -0,0 +1,20 @@
+runApplication()
+    {
+        console.log("hello world");
+    }
+
+    newClassFunction()
+    {
+        console.log("");
+    }
+
+    anotherFunctionWithArguments(aArgument)
+    {
+        console.log("kom maar met je argumanet");
+        console.log("hier:");
+        console.log(aArgument);
+    }
+
+
+let app = new App();
+app.newClassFunction();
